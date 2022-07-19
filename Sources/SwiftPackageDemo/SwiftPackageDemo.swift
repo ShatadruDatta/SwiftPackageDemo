7357b365de7e3c30a153266c16fa2476e246f370
Shatadru Datta <shatadru.datta.contractor@pepsico.com>
2022-07-19 20:43:45 +0530

    commit

--- Sources/SwiftPackageDemo/SwiftPackageDemo.swift
+++ Sources/SwiftPackageDemo/SwiftPackageDemo.swift
@@ -1,6 +1,17 @@
-public struct SwiftPackageDemo {
+import SwiftUI
+
+@available(macOS 10.15.0, *)
+public struct BarChartView: View {
     public private(set) var text = "Hello, World!"
 
-    public init() {
+    public init() { }
+    
+    public var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                Text("Hello")
+                Text("Shatadru")
+            }
+        }
     }
 }
